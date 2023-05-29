badd615198822d18979bedbd897ac11ecb986e17
ephearnyi <ifeanyi42730@gmail.com>
2023-05-29 13:17:09 -0700

    Exceptional

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,12 +1,12 @@
-#include <python.h>
+#include <Python.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
 
 /**
- * print_python_list - Python basic info
- * @n: A pyobject list
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
  */
 void print_python_list(PyObject *p)
 {
